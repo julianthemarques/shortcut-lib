7f6f048055e6bcf1039e30764879d770fcbc706b
Juliano Marques <juliano.lima.marques@outlook.com>
2025-10-05 17:24:40 -0300

    formats new configurations

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,11 @@
-// tsup.config.ts
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,            // gera .d.ts (declarations)
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
   sourcemap: true,
   clean: true,
-  external: ['react', 'react-dom'], // deixar React como peer
-  splitting: false,     // normalmente false para bibliotecas
+  external: ["react", "react-dom"],
+  splitting: false,
 });
