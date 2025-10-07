ec8da4199770d6cded36fa36a7b5f53b28420aa0
Juliano Marques <juliano.lima.marques@outlook.com>
2025-10-07 10:27:59 -0300

    renames ShortcutProps interface name

--- src/Shortcut.tsx
+++ src/Shortcut.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { IShortcutOptions, useShortCut } from "./use-shortcut";
 
-type ShortcutProps = {
+type IShortcutProps = {
     children?: React.ReactNode;
     keys: string | string[];
     onKey: (key: string, ev: KeyboardEvent) => void;
     options?: IShortcutOptions;
 };
 
-export const Shortcut = ({ children, keys, onKey, options }: ShortcutProps) => {
+export const Shortcut = ({ children, keys, onKey, options }: IShortcutProps) => {
     const shortcutHandler = useShortCut(keys, onKey, options);
 
     return (
